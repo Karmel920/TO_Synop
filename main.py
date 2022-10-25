5559624493410e57e8bbbc3bcf5fcdf71abbc447
Karmel920 <mikolajpe2@gmail.com>
2022-10-25 16:45:48 +0200

    test

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+
+def siema(name):
+    print(f'Siema {name}')
+
 if __name__ == '__main__':
     print_hi('PyCharm')
+    siema('Kamil')
 
 
